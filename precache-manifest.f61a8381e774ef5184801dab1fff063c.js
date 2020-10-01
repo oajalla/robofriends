591455d8fa8230e95ca6b2179d4a85f5cab2a5b4
rakzox <69593535+rakzox@users.noreply.github.com>
2020-10-01 12:56:07 +1000

    Updates

---
+++ precache-manifest.f61a8381e774ef5184801dab1fff063c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8b50c00959c3d06417f3a67fb36e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "01d8a60fd67f184987be",
+    "url": "/static/css/main.79ba771c.chunk.css"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/static/js/2.907bdc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.907bdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d8a60fd67f184987be",
+    "url": "/static/js/main.0e642485.chunk.js"
+  },
+  {
+    "revision": "c1e11704814894dbd6c3",
+    "url": "/static/js/runtime-main.d470ab75.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
